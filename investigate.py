5badb2ba2e4ef44607a38595834b4b45588eb1ec
chum8 <chum8@outlook.com>
2018-10-09 21:55:59 -0400

    Second commit

--- investigate.py
+++ investigate.py
@@ -7,7 +7,11 @@ def grab_data(my_file, my_error):
         exit()
     return the_file
 
+re1 = '[hack][steal][exploit][compromise]'
 my_file = 'data.txt'
 my_error ='\nExiting application'
+my_re = re.compile(re1)
 
 print('Welcome to The Forgotten Regex Lab.')
+data = grab_data(my_file, my_error)
+
