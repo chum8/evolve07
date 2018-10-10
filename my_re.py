86683b8a303481b5e5cbcd953c852c56283823f5
chum8 <chum8@outlook.com>
2018-10-10 14:45:58 -0400

    Add my_re.py to scrape domain data and results

--- my_re.py
+++ my_re.py
@@ -1,12 +1,17 @@
+# [---IMPORT LIBRARIES---]
 import re
+
+# [---CLASS DEFINITION---]
 class re_builder():
 
     # initialize class
     def __init__(self):
         # public variables
-        self.data_file = 'data.txt'
+        # self.data_file = 'data.txt'
+        self.data_file = 'universities.txt'
         self.word_list_file = 'words_01.txt'
         self.results = ""
+        self.write_file = 're_results.txt'
 
         # private variables
         self.__re = ""
@@ -30,6 +35,10 @@ class re_builder():
             # print(self.words) # debug line
         except:
             print("There was a problem loading the word list file",self.default_error)
+
+    # load a prebuilt regular expression
+    def load_re(self, my_re):
+        self.__re = my_re
 
     # build regular expression
     def build_word_search_re(self, case_sensitive = 'y'):
@@ -65,17 +74,39 @@ class re_builder():
     def retrieve_re(self):
         return self.__re
 
-# program body
+# [----PROGRAM BODY----]
+# some regex presets
+domain_re1 = '\(\w+.\w+.\w+\)'
+
+# instantiate class and load file with data to run re against
 my_re = re_builder()
 my_re.load_data()
-my_re.load_word_list()
-my_re.build_word_search_re()
+
+# for regular expressions based on a preloaded regular expression
+my_re.load_re(domain_re1)
+
+# for regular expressions based on a word list
+# my_re.load_word_list()
+# my_re.build_word_search_re()
+
+# interactive terminal
 print('Welcome to The Forgotten Five Regex Lab.')
 print('Regular expression now in memory is \'' + my_re.retrieve_re() + '\'')
 if input('Run this regular expression against \'' + my_re.data_file + '\'? (Yn) ').lower() == 'y':
     my_re.process_re()
+
+    # to print results in terminal
     for item in my_re.results:
         print(item)
 
+    # to write to file
+    with open(my_re.write_file, 'w') as f:
+        for item in my_re.results:
+            # to write stripping paranthese
+            f.write(item.translate({ord(c): None for c in '()'}))
 
+            # to write as is
+            # f.write(item)
 
+            # add new line
+            f.write('\n')
